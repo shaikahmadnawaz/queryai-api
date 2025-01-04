35a6c50b0ba7827d51b91f029c70acad83b6c58c
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2025-01-04 19:20:36 +0530

    feat: search online using gemini google search

--- services/search.py
+++ services/search.py
@@ -1,4 +1,5 @@
 from google import genai
+from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
 from config import Settings
 
 settings = Settings()
@@ -6,6 +7,19 @@ settings = Settings()
 class Search:
   def web_search(self, query: str):
     client = genai.Client(api_key=settings.GEMINI_API_KEY)
-    response = client.models.generate_content(model='gemini-2.0-flash-exp', contents=[query])
-    print(response.text)
-   
+    model_id = "gemini-2.0-flash-exp"
+    google_search_tool = Tool(
+        google_search=GoogleSearch()
+    )
+
+    response = client.models.generate_content(
+        model=model_id,
+        contents=[query],
+        config=GenerateContentConfig(
+            tools=[google_search_tool],
+            response_modalities=["TEXT"],
+        )
+    )
+
+    for each in response.candidates[0].content.parts:
+      print(each.text)
